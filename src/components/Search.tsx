c0ec2eb2a891231d2325d5d69a886a0acd006063
Nikita <nikita456nikita@yandex.ru>
2024-07-08 02:13:33 +0300

    feat/fix: add loader and reset button and fix ls function

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -11,6 +11,11 @@ export default class Search extends Component<Props> {
   state = {
     input: '',
     error: '',
+    loading: false,
+  };
+
+  setRequestToLS = (request: string) => {
+    localStorage.setItem('lastRequest', request);
   };
 
   fetchData = (request: string) => {
@@ -20,13 +25,14 @@ export default class Search extends Component<Props> {
       })
       .then((data) => {
         this.props.handlePeople(JSON.stringify(data));
+        this.setState({ loading: false });
       });
   };
 
   handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    localStorage.setItem(
-      'lastRequest',
+    this.setState({ loading: true });
+    this.setRequestToLS(
       `https://swapi.dev/api/people/?search=${this.state.input}`
     );
     this.fetchData(`https://swapi.dev/api/people/?search=${this.state.input}`);
@@ -39,27 +45,45 @@ export default class Search extends Component<Props> {
   };
 
   componentDidMount(): void {
+    this.setState({ loading: true });
     if (localStorage.getItem('lastRequest')) {
       this.fetchData(`${localStorage.getItem('lastRequest')}`);
+    } else {
+      this.fetchData(
+        `https://swapi.dev/api/people/?search=${this.state.input}`
+      );
     }
   }
 
   render(): ReactNode {
     if (this.state.error) throw new Error();
-    return (
-      <>
-        <form onSubmit={(e: FormEvent) => this.handleSubmit(e)}>
-          <input
-            type="search"
-            placeholder="Search ..."
-            onChange={(e: ChangeEvent) => this.handleInput(e)}
-          />
-          <button type="submit">Search</button>
-        </form>
-        <button onClick={() => this.setState({ error: 'yes' })}>
-          Throw an error
-        </button>
-      </>
-    );
+    if (this.state.loading) {
+      return <p>Loading...</p>;
+    } else
+      return (
+        <div>
+          <form onSubmit={(e: FormEvent) => this.handleSubmit(e)}>
+            <input
+              type="search"
+              placeholder="Search ..."
+              onChange={(e: ChangeEvent) => this.handleInput(e)}
+              required={true}
+            />
+            <button type="submit">Search</button>
+            <button
+              type="button"
+              onClick={() => {
+                this.fetchData(`https://swapi.dev/api/people/`);
+                this.setRequestToLS(`https://swapi.dev/api/people/`);
+              }}
+            >
+              reset
+            </button>
+          </form>
+          <button onClick={() => this.setState({ error: 'yes' })}>
+            Throw an error
+          </button>
+        </div>
+      );
   }
 }
