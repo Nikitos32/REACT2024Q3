0b6bbb93a5cc969b17fe54369a7d79e708dd7cf4
Nikita <nikita456nikita@yandex.ru>
2024-07-07 23:49:40 +0300

    feat: add search logic and handle state function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Component, ReactNode } from 'react';
+import Search from './components/Search';
 
 export default class App extends Component {
   render(): ReactNode {
-    return <></>;
+    return <Search />;
   }
 }
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,30 @@
-import { Component, ReactNode } from 'react';
+import { ChangeEvent, Component, FormEvent, ReactNode } from 'react';
 
 export default class Search extends Component {
+  state = {
+    input: '',
+  };
+
+  handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  handleInput = (e: ChangeEvent) => {
+    this.setState({
+      input: (e.target as HTMLInputElement).value,
+    });
+  };
+
   render(): ReactNode {
-    return <></>;
+    return (
+      <form onSubmit={(e: FormEvent) => this.handleSubmit(e)}>
+        <input
+          type="search"
+          placeholder="Search ..."
+          onChange={(e: ChangeEvent) => this.handleInput(e)}
+        />
+        <button type="submit">Search</button>
+      </form>
+    );
   }
 }
