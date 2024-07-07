4979fca8c74ff39a139a00b5ee26d80906a04063
Nikita <nikita456nikita@yandex.ru>
2024-07-08 00:33:17 +0300

    feat: pass props to the children components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,22 @@ import { Component, ReactNode } from 'react';
 import Search from './components/Search';
 
 export default class App extends Component {
+  state = {
+    starWarsPeople: '',
+  };
+
+  handlePeople = (people: string) => {
+    this.setState({
+      starWarsPeople: people,
+    });
+  };
+
   render(): ReactNode {
-    return <Search />;
+    return (
+      <Search
+        people={this.state.starWarsPeople}
+        handlePeople={this.handlePeople}
+      />
+    );
   }
 }
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,26 @@
 import { ChangeEvent, Component, FormEvent, ReactNode } from 'react';
 
-export default class Search extends Component {
+interface SearchProps {
+  people: string;
+  handlePeople: (people: string) => void;
+}
+
+type Props = Readonly<SearchProps>;
+
+export default class Search extends Component<Props> {
   state = {
     input: '',
   };
 
   handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    fetch(`https://swapi.dev/api/people/?search=${this.state.input}`)
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        this.props.handlePeople(JSON.stringify(data));
+      });
   };
 
   handleInput = (e: ChangeEvent) => {
@@ -24,6 +38,7 @@ export default class Search extends Component {
           onChange={(e: ChangeEvent) => this.handleInput(e)}
         />
         <button type="submit">Search</button>
+        <p>{this.props.people}</p>
       </form>
     );
   }
