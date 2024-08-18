fb622d853c0f7aa89e98012a904c1ef48f53c822
Nikita <nikita456nikita@yandex.ru>
2024-08-18 23:07:31 +0300

    refactor: delete tests

--- src/tests/ErrorPage.test.tsx
+++
@@ -1,7 +0,0 @@
-import ErrorPage from '../../app/not-found';
-import { render, screen } from '@testing-library/react';
-
-test('renders a message', () => {
-  render(<ErrorPage />);
-  expect(screen.getByText('404 PAGE NOT FOUND')).toBeDefined();
-});
